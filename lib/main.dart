a3016a0f2123248a98216ad3265111d6fec2fe4c
Muhammad <mudevelop0@gmail.com>
2019-10-15 17:04:45 +0200

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import './utility/shared_prefs.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Image Shared Prefs',
+      home: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Image image;
+
+  pickImage(ImageSource source) async {
+    final _image = await ImagePicker.pickImage(source: source);
+
+    if (_image != null) {
+      setState(() {
+        image = Image.file(_image);
+      });
+      ImageSharedPrefs.saveImageToPrefs(
+          ImageSharedPrefs.base64String(_image.readAsBytesSync()));
+    } else {
+      print('Error picking image!');
+    }
+  }
+
+  loadImageFromPrefs() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final imageKeyValue = prefs.getString(IMAGE_KEY);
+    if (imageKeyValue != null) {
+      final imageString = await ImageSharedPrefs.loadImageFromPrefs();
+      setState(() {
+        image = ImageSharedPrefs.imageFrom64BaseString(imageString);
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadImageFromPrefs();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Image Shared Preferences'),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: image == null ? Container() : image,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add_a_photo),
+        onPressed: () {
+          showModalBottomSheet<void>(
+              context: context,
+              builder: (BuildContext context) {
+                return SafeArea(
+                  child: new Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      new ListTile(
+                        leading: new Icon(Icons.camera),
+                        title: new Text('Camera'),
+                        onTap: () {
+                          pickImage(ImageSource.camera);
+                          // this is how you dismiss the modal bottom sheet after making a choice
+                          Navigator.pop(context);
+                        },
+                      ),
+                      new ListTile(
+                        leading: new Icon(Icons.image),
+                        title: new Text('Gallery'),
+                        onTap: () {
+                          pickImage(ImageSource.gallery);
+                          // dismiss the modal sheet
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ],
+                  ),
+                );
+              });
+        },
+      ),
+    );
+  }
+}
---
+++ lib/utility/shared_prefs.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:flutter/widgets.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+const IMAGE_KEY = 'IMAGE_KEY';
+
+class ImageSharedPrefs {
+  static Future<bool> saveImageToPrefs(String value) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    return await prefs.setString(IMAGE_KEY, value);
+  }
+
+  static Future<String> loadImageFromPrefs() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString(IMAGE_KEY);
+  }
+
+  // encodes bytes list as string
+  static String base64String(Uint8List data) {
+    return base64Encode(data);
+  }
+
+  // decode bytes from a string
+  static imageFrom64BaseString(String base64String) {
+    return Image.memory(
+      base64Decode(base64String),
+      fit: BoxFit.fill,
+    );
+  }
+}
